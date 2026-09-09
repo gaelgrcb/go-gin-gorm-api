5eeddb8285f107900809e8df9ccecfb6b560a72f
gaelgrcb <gaelgarciabst@gmail.com>
2026-09-08 18:28:58 -0700

    feat: add gorm postgres connection

---
+++ pkg/database/database.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DBConfig struct {
+	Host string `mapstructure:"DB_HOST"`
+	Port int    `mapstructure:"DB_PORT"`
+	User string `mapstructure:"DB_USER"`
+	Pass string `mapstructure:"DB_PASS"`
+	Name string `mapstructure:"DB_NAME"`
+}
+
+func Connect(cfg DBConfig) (*gorm.DB, error) {
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		cfg.Host, cfg.Port, cfg.User, cfg.Pass, cfg.Name)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger:                 logger.Default.LogMode(logger.Info),
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get sql.DB: %w", err)
+	}
+
+	// Pool settings
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	return db, nil
+}
