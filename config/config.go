9fe6d32956f3a39aa0c71d5c714c45d524a290af
gaelgrcb <gaelgarciabst@gmail.com>
2026-09-08 18:29:03 -0700

    feat: add viper configuration loader

---
+++ config/config.go
@@ -0,0 +1,47 @@
+package configs
+
+import (
+	"CRUD/pkg/database"
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AppName string `mapstructure:"APP_NAME"`
+	// Server
+	AppEnv  string `mapstructure:"APP_ENV"`
+	AppPort string `mapstructure:"APP_PORT"`
+
+	//DB
+	DB database.DBConfig `mapstructure:",squash"`
+
+	// Security
+	JWTSecret       string `mapstructure:"JWT_SECRET"`
+	JWTExpiresHours int    `mapstructure:"JWT_EXPIRES_HOURS"`
+
+	// CORS for Frontend
+	CORSAllowedOrigins string `mapstructure:"CORS_ALLOWED_ORIGINS"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error reading config file, %s", err)
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, fmt.Errorf("unable to decode config, %v", err)
+	}
+
+	if config.DB.Host == "" || config.DB.Name == "" {
+		return nil, fmt.Errorf("error some configs are missing or do not match")
+	}
+
+	return &config, nil
+}
